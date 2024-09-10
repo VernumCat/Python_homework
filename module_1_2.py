276635c8cf98676005bfe833f7aaf5bde2340301
VernumCat <iqnism@gmail.com>
2024-09-10 17:17:05 +0300

    10.09.2024

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+homework = 12
+hour = 1.5
+course = 'Python'
+time = hour / homework
+print(f'Курс {course}, всего задач: {homework}, затрачено часов: {hour}, '
+      f'среднее время выполнения: {time} часа.')
---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Маргарита'
+print(f'Name {name}')
+age = 36
+print(f'Age: {age}')
+age = 37
+print(f'New age: {age}')
+is_student = True
+print(f'Is student {is_student}')
