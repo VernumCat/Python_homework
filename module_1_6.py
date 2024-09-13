adeb3cf8358025ecd0277265387f13ec33b4325c
VernumCat <iqnism@gmail.com>
2024-09-14 00:08:32 +0300

    14.09.2024

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+# работа со словарем
+my_dict = {'Oksana': 1994, 'Igor': 2004, 'Klara': 2024}
+print('Dict:', my_dict)
+print('Existing value:', my_dict.get('Oksana'))
+print('Not existing value:', my_dict.get('Alex'))
+my_dict.update({'Eva': 2000, 'Egor': 2001})
+print('Deleted value:', my_dict.pop('Klara'))
+print('Modified dictionary:', my_dict)
+# работа со множеством
+print('')
+my_set = {1, 2, 3, 1, 2, 3, 'fool'}
+print('Set:', my_set)
+my_set.add(3.14)
+my_set.add(5.15)
+my_set.discard(3)
+print('Modified set:', my_set)
