d0ce133adb67404927df79ca77a73ccb7480c6a9
VernumCat <iqnism@gmail.com>
2024-09-13 18:05:33 +0300

    13.09.2024

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = tuple([10, 20, True, False, 'кортеж'])
 print('immutable_var:', immutable_var)
-#immutable_var [2] = False #переменную внутри кортежа по правилам изменить нельзя иначе программа будет выдвать ошибку
+#immutable_var [2] = False #данная операция не поддерживается, программа выдает ошибку
 mutable_list = [10, 10, True, False, 'список']
 mutable_list[1] = 20
 print('mutable_list:', mutable_list)
