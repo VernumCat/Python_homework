4727cff129343021beca8b92c6b9b3526cc94ae4
VernumCat <iqnism@gmail.com>
2024-09-20 21:29:30 +0300

    20.09.2024

--- module_2_4.py
+++ module_2_4.py
@@ -1,14 +1,13 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in numbers:
-    for x in [2, 3, 4, 5, 6, 7, 8, 9]:
+for i in numbers[1:]:
+    for x in numbers[1:i]:
         if i % x == 0 and i != x:
-            not_primes.append(i)
-            break
-        else:
-            primes.append(i)
-            break
+            if i not in not_primes:
+                not_primes.append(i)
+    if i not in not_primes and i not in primes:
+        primes.append(i)
 print('Primes:', primes)
 print('Not primes:', not_primes)
 
